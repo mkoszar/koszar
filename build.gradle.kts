52dbe381b0a09f9e901dfc9745d2be85d5836cd6
mkoszar <koszar@gmail.com>
2025-12-21 11:44:15 +0100

    Fix Gradle plugin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
-plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.6.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
